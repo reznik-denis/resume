46a2fd4411d3d794ef71a5a548dff775025a1a1d
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2021-11-20 12:51:17 +0200

    add logik email sand

--- js/index.js
+++ js/index.js
@@ -2,28 +2,34 @@ const refs = {
     openModel: document.querySelector('.open-modal-team'),
     showModel: document.querySelector('.js-backdrop'),
     closeModelBtn: document.querySelector('.close-modal-btn'),
+    submit: document.querySelector('.submit-form'),
+    name: document.querySelector('#name'),
+    tel: document.querySelector('#tel'),
+    message: document.querySelector('#message'),
+    form: document.querySelector(".js-speaker-form"),
    }
 
-// Открытие модального окна
+
 refs.openModel.addEventListener('click', onOpenModal);
+refs.closeModelBtn.addEventListener('click', onCloseModal);
+refs.showModel.addEventListener('click', onCloseModalByClickBackdrop);
 
 function onOpenModal(e) {
     e.preventDefault();
     refs.showModel.classList.add('is-open');
   window.addEventListener('keydown', onCloseModalByEscape);
-  document.body.classList.add('modal-open');
+    document.body.classList.add('modal-open');
+    refs.submit.addEventListener('click', submitForm);
+    refs.form.addEventListener('submit', submitForm);
 }
-
-// Закрытие модального окна
-refs.closeModelBtn.addEventListener('click', onCloseModal);
 
 function onCloseModal() {
   refs.showModel.classList.remove('is-open'); 
-  window.removeEventListener('keydown', onCloseModalByEscape);
-  document.body.classList.remove('modal-open');
+    window.removeEventListener('keydown', onCloseModalByEscape);
+    refs.submit.removeEventListener('click', submitForm);
+    refs.form.removeEventListener('submit', submitForm);
+    document.body.classList.remove('modal-open');
 }
-
-refs.showModel.addEventListener('click', onCloseModalByClickBackdrop);
 
 function onCloseModalByClickBackdrop(evt) {
   if (evt.currentTarget === evt.target) {
@@ -31,7 +37,6 @@ function onCloseModalByClickBackdrop(evt) {
   }
 }
 
-// Закрытие модального окна по нажатию клавиши ESC.
 function onCloseModalByEscape(evt) {
   const ESC_KEY_CODE = 'Escape';
   const isEscKey = evt.code === ESC_KEY_CODE;
@@ -40,3 +45,32 @@ function onCloseModalByEscape(evt) {
     onCloseModal();
   }
 }
+
+function submitForm(e) {
+    e.preventDefault();
+    if (refs.name.value === '' || refs.name.value === ' ') {
+        alert('Enter the name');
+        return
+    } else if (refs.tel.value === '' || refs.tel.value === ' ') {
+        alert('Enter the tel');
+        return
+    } else if (refs.message.value === '' || refs.message.value === ' ') {
+        alert('Enter the massage');
+        return
+    } else {
+        const form = {
+            name: refs.name.value,
+            telephone: refs.tel.value,
+            message: refs.message.value,
+        }
+        window.open(`mailto:reznik.d.o12@gmail.com?subject=Message for Denis&body=Name: ${form.name} Telephone: ${form.telephone} Message: ${form.message}`)
+    }
+    reset();
+}
+
+function reset() {
+    refs.name.value = '';
+    refs.tel.value = '';
+    refs.message.value = '';
+}
+  
