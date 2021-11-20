7c35cf7d30aea10f2336d69ca70462b051663395
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2021-11-20 10:59:53 +0200

    add all desctop css

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const refs = {
+    openModel: document.querySelector('.open-modal-team'),
+    showModel: document.querySelector('.js-backdrop'),
+    closeModelBtn: document.querySelector('.close-modal-btn'),
+   }
+
+// Открытие модального окна
+refs.openModel.addEventListener('click', onOpenModal);
+
+function onOpenModal(e) {
+    e.preventDefault();
+    refs.showModel.classList.add('is-open');
+  window.addEventListener('keydown', onCloseModalByEscape);
+  document.body.classList.add('modal-open');
+}
+
+// Закрытие модального окна
+refs.closeModelBtn.addEventListener('click', onCloseModal);
+
+function onCloseModal() {
+  refs.showModel.classList.remove('is-open'); 
+  window.removeEventListener('keydown', onCloseModalByEscape);
+  document.body.classList.remove('modal-open');
+}
+
+refs.showModel.addEventListener('click', onCloseModalByClickBackdrop);
+
+function onCloseModalByClickBackdrop(evt) {
+  if (evt.currentTarget === evt.target) {
+    onCloseModal();
+  }
+}
+
+// Закрытие модального окна по нажатию клавиши ESC.
+function onCloseModalByEscape(evt) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = evt.code === ESC_KEY_CODE;
+
+  if (isEscKey) {
+    onCloseModal();
+  }
+}
